f418636ad94bb367e3ac5a825ecd8eb6cbf4c06c
Laksh Mendpara <142152949+Laksh-Mendpara@users.noreply.github.com>
2024-04-21 16:14:19 +0530

    Update model_saver.h

--- include/model_saver.h
+++ include/model_saver.h
@@ -6,4 +6,7 @@ void save_model(layer_component* self);
 
 void load_model(layer_component* self);
 
+void init_model_architecture(layer_component** linput, layer_component** lconv1, layer_component** lconv2,
+                                layer_component** lfull1, layer_component** lfull2, layer_component** loutput);
+
 #endif
